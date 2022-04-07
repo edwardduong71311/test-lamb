0fa8325cb361cb7b68a7f8ad520cad942d1a13c6
Edward Duong <amen71311@gmail.com>
2022-04-07 12:28:20 +0700

    Finished ad lib

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+import { format } from 'date-fns';
+
 exports.handler = async (event) => {
   // TODO implement
   const response = {
       statusCode: 200,
-      body: JSON.stringify('Edward push code from Github'),
+      body: JSON.stringify('Today is: ' + format(new Date(), 'dd/MM/yyyy hh:mm:ss aa')),
   };
   return response;
 };
